289a7be4aa7b4e52ecac3ad0f64a597d22eda4a7
Lukas Korous <lukas.korous@gmail.com>
2016-09-26 00:41:48 +0200

    Fix APK build

--- release-build.bat
+++ release-build.bat
@@ -1,3 +1,9 @@
 gulp
+rm -rf www/js
+rm -rf www/css
+rm -rf www/templates
+rm -rf www/index-dev.html
+rm -rf www/index-min.html
 cordova build android --release -- --keystore=my-release-key.keystore --alias=SCHEDULOGY
 cp platforms/android/build/outputs/apk/android-release.apk SCHEDULOGY.apk
+git checkout .
