f81d02eca6492a11f1ca49a1339306613106e7ae
Lukas Korous <lukas.korous@gmail.com>
2016-10-04 22:39:03 +0200

    Fix stuff.

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('concat-js', function () {
         "www/js/services/auth.js",
         "www/js/services/fileUpload.js",
         "www/js/services/notification.js",
-        "www/js/services/cordova.js",
+        "www/js/services/cordovaUtilities.js",
         "www/js/services/myEvents.js",
         "www/js/services/myUsers.js",
         "www/js/services/myResources.js",
