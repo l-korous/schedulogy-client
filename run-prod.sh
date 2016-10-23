65e598c7ba0ed0beb026f7800d1d5036f10c819b
Lukas Korous <lukas.korous@gmail.com>
2016-10-23 10:57:43 +0200

    Fixes.

--- run-prod.sh
+++ run-prod.sh
@@ -1,3 +1,4 @@
 git checkout .
+git pull origin master
 cp www/js/settings-prod.js www/js/settings.js
 gulp
--- run-test.sh
+++ run-test.sh
@@ -1,2 +1,3 @@
 git checkout .
+git pull origin master
 cp www/js/settings-test.js www/js/settings.js
