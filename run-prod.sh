17882340f20ac8a9c1cf23453f16bf18cb9d1e7f
Lukas Korous <lukas.korous@gmail.com>
2016-12-31 13:25:09 +0100

    Add bower

--- run-prod.sh
+++ run-prod.sh
@@ -1,8 +1,4 @@
 git checkout .
 git pull origin master
 cp www/js/settings-prod.js www/js/settings.js
-cd www
-bower install
-bower update
-cd ..
 gulp
--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,3 @@
 git checkout .
 git pull origin master
 cp www/js/settings-test.js www/js/settings.js
-cd www
-bower install
-bower update
