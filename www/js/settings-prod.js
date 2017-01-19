ad56cdc2859a7f6ab9f3df4b479dc5954d7e3162
Lukas Korous <lukas.korous@gmail.com>
2017-01-19 20:35:28 +0100

    2.5.0

--- www/js/settings-prod.js
+++ www/js/settings-prod.js
@@ -3,7 +3,7 @@ angular.module('Schedulogy')
         serverUrl: 'https://www.schedulogy.com/api',
         AUTH0_CLIENT_ID: 'hq0XyOBL2u40XepMg7ZyHT4X2NsrkKt0',
         AUTH0_DOMAIN: 'schedulogy.eu.auth0.com',
-        appVersion: '2.4.0',
+        appVersion: '2.5.0',
         fixedBTime: {
             on: false,
             date: 'Sat Sep 03 2016 12:00:00 GMT+0200'
